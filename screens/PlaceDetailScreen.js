2a6fc722f0e06abcc5eabb60f3c62b16b5865fa4
bud linville <budlinville@gmail.com>
2021-03-17 10:47:12 -0500

    Added initial location and readonly functionality to MapScreen

--- screens/PlaceDetailScreen.js
+++ screens/PlaceDetailScreen.js
@@ -9,6 +9,13 @@ const PlaceDetailScreen = props => {
   const placeId = props.navigation.getParam('placeId');
   const selectedPlace = useSelector(state =>
     state.places.places.find(place => place.id === placeId));
+
+  const selectedLocation = { lat: selectedPlace.lat, lng: selectedPlace.lng };
+
+  const showMapHandler = () => {
+    props.navigation.navigate('Map', { readonly: true, initialLocation: selectedLocation });
+  };
+
   return (
     <ScrollView contentContainerStyle={{ alignItems: 'center' }}>
       <Image source={{ uri: selectedPlace.imageUri }} style={styles.image}/>
@@ -18,7 +25,8 @@ const PlaceDetailScreen = props => {
         </View>
         <MapPreview
           style={styles.mapPreview}
-          location={{ lat: selectedPlace.lat, lng: selectedPlace.lng }} />
+          location={selectedLocation}
+          onPress={showMapHandler}/>
       </View>
     </ScrollView>
   );
